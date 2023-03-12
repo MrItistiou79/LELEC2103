0ebd05a246e6569979f9ed147fcf3989f1295188
MrItistiou79 <nordin.trifiro@hotmail.be>
2023-03-12 15:47:39 +0100

    norm : works up to matrix multiplication

--- mcu/Core/Src/spectrogram.c
+++ mcu/Core/Src/spectrogram.c
@@ -34,44 +34,36 @@ void Spectrogram_Format(q15_t *buf)
 // Compute spectrogram of samples and transform into MEL vectors.
 void Spectrogram_Compute(q15_t *samples, q15_t *melvec)
 {
-	// melvec = vecteur de retour dans lequel on met le résultat de la fft * melvec
-	// fft vecteur = 256
-
 	// STEP 1  : Windowing of input samples
-	arm_mult_q15(samples, hamming_window, buf, SAMPLES_PER_MELVEC);
 
-	// STEP 2  : Discrete Fourier Transform
-	arm_rfft_instance_q15 rfft_inst;
-	arm_rfft_init_q15(&rfft_inst, SAMPLES_PER_MELVEC, 0, 1);
-	arm_rfft_q15(&rfft_inst, buf, buf_fft);
+		arm_mult_q15(samples, hamming_window, buf, SAMPLES_PER_MELVEC);
+
+		// STEP 2  : Discrete Fourier Transform
+		arm_rfft_instance_q15 rfft_inst;
+		arm_rfft_init_q15(&rfft_inst, SAMPLES_PER_MELVEC, 0, 1);
+		arm_rfft_q15(&rfft_inst, buf, buf_fft);
+
+//
+//		arm_matrix_instance_q15 hz2mel_inst, fftmag_inst, melvec_inst;
+//
+//		arm_mat_init_q15(&hz2mel_inst, MELVEC_LENGTH, SAMPLES_PER_MELVEC/2, hz2mel_mat);
+//		arm_mat_init_q15(&fftmag_inst, SAMPLES_PER_MELVEC/2, 1, buf);
+//		arm_mat_init_q15(&melvec_inst, MELVEC_LENGTH, 1, melvec);
+//
+//		arm_mat_mult_fast_q15(&hz2mel_inst, &fftmag_inst, &melvec_inst, buf_tmp);
+
+	//------------------------------------------------------
+	// new method : start
+	//------------------------------------------------------
 
 	//computation of an estimation of the norm
-	q15_t alpha = 0.96043387;
-	q15_t beta  = 0.39782473;
-
 	for (int i = 0; i < (uint16_t) (SAMPLES_PER_MELVEC/2); i++){
-		buf[i] = alpha * MAX(buf_fft[2*i], buf_fft[2*i+1]) + beta * MIN(buf_fft[2*i], buf_fft[2*i + 1]);
+		buf[i] = (q15_t) (0.96043387 * ABS(MAX(buf_fft[2*i], buf_fft[2*i+1])) + 0.39782473 * ABS(MIN(buf_fft[2*i], buf_fft[2*i + 1])));
 	}
 
-	// multiply by the mel matrix
-
-
-	// STEP 4:   Apply MEL transform
-	//           --> Fast Matrix Multiplication
-	//           Complexity: O(Nmel*N)
-	//           Number of cycles: <TODO>
-
-	// /!\ The difference between the function arm_mat_mult_q15() and the fast variant is that the fast variant use a 32-bit rather than a 64-bit accumulator.
-	// The result of each 1.15 x 1.15 multiplication is truncated to 2.30 format. These intermediate results are accumulated in a 32-bit register in 2.30 format.
-	// Finally, the accumulator is saturated and converted to a 1.15 result. The fast version has the same overflow behavior as the standard version but provides
-	// less precision since it discards the low 16 bits of each multiplication result.
-
-	// /!\ In order to avoid overflows completely the input signals should be scaled down. Scale down one of the input matrices by log2(numColsA) bits to avoid overflows,
-	// as a total of numColsA additions are computed internally for each output element. Because our hz2mel_mat matrix contains lots of zeros in its rows, this is not necessary.
-	
 	arm_matrix_instance_q15 melvec_inst;
 
-	// transforms the vectors into matrix
+	//transforms the vectors into matrix
 	arm_mat_init_q15(&melvec_inst, MELVEC_LENGTH, 1, melvec);
 
 
@@ -80,11 +72,9 @@ void Spectrogram_Compute(q15_t *samples, q15_t *melvec)
 		uint8_t curr 	 = *(ind + 2*i);
 		uint8_t nbr_elem = *(ind + 2*i + 1);
 
-		// computation of the ind elem of the melvec
+		 //computation of the ind elem of the melvec
 		for(uint8_t j; j < nbr_elem; j++){
 			melvec_inst.pData[i] += buf[curr%256 + j] * hz2mel_mat[curr + j];
 		}
 	}
-	// multipl
-	//arm_mat_mult_fast_q15(&hz2mel_inst, &fftmag_inst, &melvec_inst, buf_tmp);
 }
